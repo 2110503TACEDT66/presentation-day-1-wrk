1b20acd8708e426a21fa17bc75bebf75be1c2a71
witchakra <131113499+witchakra@users.noreply.github.com>
2024-03-31 16:31:25 +0700

    fix

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ const server = app.listen(
 		'Server running in ',
 		process.env.NODE_ENV,
 		' mode on port ',
-		PORT
+		'on'+process.env.HOST+':'+PORT
 	)
 );
 
@@ -76,7 +76,7 @@ const swaggerOptions = {
 		},
 		servers: [
 			{
-				url: 'http://localhost:5000/api/v1',
+				url: process.env.HOST+':'+PORT+'/api/v1',
 			},
 		],
 	},
